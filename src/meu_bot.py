a4fe716327bc4163c707cc76f2928ab0add37e42
Lucas Almeida Coelho <89714517+lucasacoelho@users.noreply.github.com>
2023-05-03 23:00:15 -0300

    Update meu_bot.py

--- src/meu_bot.py
+++ src/meu_bot.py
@@ -6,11 +6,11 @@ class MyClient(discord.Client):
 
     async def on_message(self, message):
         print('Message from {0.author}: {0.content}'.format(message))
-        if message.content == '§cu':
-            await message.channel.send(f'Olá {message.author.name}! Aqui esta o cu')
-        elif message.content == '§cantada':
+        if message.content == '!go':
+            await message.channel.send(f'Olá {message.author.name}! La vamos nos!')
+        elif message.content == '!cantada':
             await message.author.send(f'Olá {message.author.name}! Barril!')
-            await message.author.send(f'vem que vou te comer la em casa')
+            await message.author.send(f'Gostou do barril?)
 
     async def on_member_join(self,member):
         guild = member.guild
